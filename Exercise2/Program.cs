f248ba38e87a9ed620a38e2ddb4e7b33cd1ad7f9
dkhanal112 <dkhanal112@gmail.com>
2020-09-24 07:43:59 +0545

    Exercise2 Completed

---
+++ Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+    class CreateDirectory
+    {
+        static void Main()
+        {
+            CreateDirectory folder = new CreateDirectory();
+            folder.CreateDirectories();
+        }
+
+        public void CreateDirectories()
+        {
+            
+        string rootFolderPath = "Nepal";
+        Directory.CreateDirectory(rootFolderPath);
+        string[] subfolders = {"Dotnet","Compiler","Construction","software","Engineering","ecommerce","egovernance","technical","writing","reading"};
+        foreach(string sub in subfolders)
+        {
+            string subfolderPath = rootFolderPath + "/" + sub;
+            Directory.CreateDirectory(subfolderPath);
+            string filePath = subfolderPath + "/" + "test.txt";
+            File.WriteAllText(filePath,sub);
+        }
+        }
+    }
