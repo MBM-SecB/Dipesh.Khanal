7fa6641e1dff039eae52e3ec0ea96c6aa02717f1
dkhanal112 <dkhanal112@gmail.com>
2021-03-05 10:25:54 +0545

    readme updaetd

--- StudentsApi/Controllers/StudentsController.cs
+++ StudentsApi/Controllers/StudentsController.cs
@@ -1,7 +1,9 @@
 
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using StudentsApi.Models;
 
-[Route("students")]
+[Route("api/students")]
 [ApiController]
 public class StudentsController: ControllerBase
 {
@@ -10,10 +12,42 @@ public class StudentsController: ControllerBase
     public ActionResult GetAllStudents()
     {
         var students = new string[]{"DIpesh", "Sagar", "Swostika"};
-     
+            
         if(students==null){
             return NotFound();
         }
         return  Ok(students); 
     }
+    [HttpGet]
+    [Route("{name}")]
+    public ActionResult GetAllStudentById(string name)
+    {
+        var students = new string[]{"Dipesh", "Sagar", "Swostika"};
+         var student=   students.Where(x=> x == name).FirstOrDefault();
+        if(student==null){
+            return NotFound();
+        }
+        return  Ok(student); 
+    }
+    [HttpPost]
+    [Route("add")]
+    public ActionResult CreateStudent(Student student )
+    {
+       
+        if(student==null){
+            return BadRequest();
+        }
+        return  Created("",student); 
+    }
+    [HttpPost]
+    [Route("delete")]
+    public ActionResult delete(Student student )
+    {
+       
+        if(student==null){
+            return BadRequest();
+        }
+        return  Created("",student); 
+    }
+ 
 }
---
+++ StudentsApi/models/Student.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace StudentsApi.Models{
+    public class Student {
+    public int id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public DateTime Dob { get; set; }
+}
+}
